3b06797991a2fd25c87dfc29d338755b11e6ba52
Huan Zhang <huazhang@redhat.com>
2013-08-30 17:25:40 +0800

    change default log level DEBUG=>INFO

--- misc/logger.py
+++ misc/logger.py
@@ -3,7 +3,7 @@
 import logging
 
 class GenLogger:
-	logging.basicConfig(level = logging.DEBUG,
+	logging.basicConfig(level = logging.INFO,
 			format   = '%(asctime)s %(filename)s %(levelname)s %(message)s',
 			datefmt  = '%a, %d %b %Y %H:%M:%S')
 
